e6dbfb246622e7bb526ac15d00b93b0652ce55fc
kauabarbosaa1728-web <kaua.barbosaa1728@gmail.com>
2026-04-07 15:07:13 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from loja import loja_bp
+
+app = Flask(__name__)
+app.secret_key = "segredo123"
+
+app.register_blueprint(loja_bp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
