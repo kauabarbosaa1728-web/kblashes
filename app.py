32158d15e57c750eb6fdbf39cd4fee8f8443bc15
kauabarbosaa1728-web <kaua.barbosaa1728@gmail.com>
2026-04-18 10:56:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from loja import loja_bp
-from servicos import servicos_bp  # 🔥 NOVA IMPORTAÇÃO
+from servicos import servicos_bp
+from valores import valores_bp  # 🔥 NOVA IMPORTAÇÃO
 
 app = Flask(__name__)
 app.secret_key = "segredo123"
 
 # 🔥 BLUEPRINTS
 app.register_blueprint(loja_bp)
-app.register_blueprint(servicos_bp)  # 🔥 REGISTRO DA NOVA PÁGINA
+app.register_blueprint(servicos_bp)
+app.register_blueprint(valores_bp)  # 🔥 REGISTRO DA PÁGINA DE VALORES
 
 # 🔥 ROTA PRA MANTER O SITE ATIVO
 @app.route("/ping")
